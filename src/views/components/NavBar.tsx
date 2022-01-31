e03a917a1cedccc7c41a3a9a6818a33585d3e21c
Tony <tonykurosaki117@gmail.com>
2022-01-30 18:08:01 -0600

    fixed navigation button

--- src/views/components/NavBar.tsx
+++ src/views/components/NavBar.tsx
@@ -1,13 +1,22 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useState } from "react";
 import '../../styles/components/Navbar/Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useNavigate } from "react-router-dom";
-import { faBars,faHome,faShoppingBag,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faBars,faHome,faShoppingBag,faShoppingCart, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { ProductContext } from "../../app/Context";
 
 export const NavBar:FC = () => {
     const history = useNavigate();
+    const [isClosed, setIsClosed] = useState<boolean>(true)
     const context = useContext(ProductContext);
+    const handleClick = (e:any) => {
+        setIsClosed(!isClosed)
+        console.log(e.target.value)
+    }
+    const handleNavigate = (route:string) => {
+        history(route)
+        setIsClosed(true)
+    }
     return(
         <>
             <header className="navbar-container">
@@ -17,18 +26,24 @@ export const NavBar:FC = () => {
                         type="checkbox" 
                         name="menu-button-checkbox" 
                         id='menu-button-checkbox'
+                        onClick={handleClick}
                     />
                     <label htmlFor="menu-button-checkbox">
-                        <FontAwesomeIcon icon={faBars} size='2x'/>
+                        {
+                            !isClosed ?
+                            <FontAwesomeIcon icon={faTimesCircle} size='2x'/>
+                            :
+                            <FontAwesomeIcon icon={faBars} size='2x'/>
+                        }
                     </label>
-                    <ul className="navbar-list">
-                        <li onClick={() => history("")}>
+                    <ul className="navbar-list" style={{'left': isClosed ? '-100%': 0 }}>
+                        <li onClick={() => handleNavigate("")}>
                             <FontAwesomeIcon icon={faHome} /> Inicio 
                         </li>
-                        <li onClick={() => history("productos")}>
+                        <li onClick={() => handleNavigate("productos")}>
                             <FontAwesomeIcon icon={faShoppingBag} /> Productos
                         </li>
-                        <li onClick={() => history("carrito-de-compras")}>
+                        <li onClick={() => handleNavigate("carrito-de-compras")}>
                             <FontAwesomeIcon icon={faShoppingCart} /> Carrito
                             <span>{context?.shoppingCart.length}</span>
                         </li>
